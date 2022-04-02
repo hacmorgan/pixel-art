822ff293404c014b46fbacb4e8f01a30ebd3f219
Hamish Morgan <ham430@gmail.com>
2022-04-02 16:31:47 +1100

    Add leakyReLUs to deeper discriminator
    This may have been why he was tending towards darkness

--- architectures.py
+++ architectures.py
@@ -23,27 +23,27 @@ def generator_model_dcgan_paper() -> tf.keras.Sequential:
             layers.Dense(4 * 4 * 1024, activation="relu"),
             layers.Reshape((4, 4, 1024)),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # layers.leakyReLU(alpha=0.2),
             layers.Conv2DTranspose(
                 512, kernel_size=5, strides=2, padding="same", activation="relu"
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # layers.leakyReLU(alpha=0.2),
             layers.Conv2DTranspose(
                 256, kernel_size=5, strides=2, padding="same", activation="relu"
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # layers.leakyReLU(alpha=0.2),
             layers.Conv2DTranspose(
                 128, kernel_size=5, strides=2, padding="same", activation="relu"
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # layers.leakyReLU(alpha=0.2),
             layers.Conv2DTranspose(
                 3, kernel_size=5, strides=2, padding="same", activation="tanh"
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # layers.leakyReLU(alpha=0.2),
         ]
     )
     model.summary()
@@ -52,33 +52,41 @@ def generator_model_dcgan_paper() -> tf.keras.Sequential:
 
 def generator_model_dcgan_paper_lite() -> tf.keras.Sequential:
     """
-    The generator used in the DCGAN paper.
+    Based on the generator used in the DCGAN paper.
 
     A smaller model, to fit on a GTX980.
     """
     latent_dim = 100
+    init = tf.keras.initializers.RandomNormal(stddev=0.02)
     model = tf.keras.Sequential(
         [
+            # Latent input
             layers.Input(shape=(latent_dim,)),
-            layers.Dense(8 * 8 * 512, use_bias=False, activation="relu"),
+            # Dense layer, shaped as (8, 8, 512) conv layer
+            layers.Dense(8 * 8 * 512, kernel_initializer=init),
+            layers.leakyReLU(alpha=0.2),
             layers.Reshape((8, 8, 512)),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            # 1st upscale by fractionally strided conv, (16, 16, 256)
             layers.Conv2DTranspose(
-                256, kernel_size=5, strides=2, padding="same", activation="relu", use_bias=False,
+                256, kernel_size=5, strides=2, padding="same", kernel_initializer=init,
+            ),
+            layers.leakyReLU(alpha=0.2),
+            layers.BatchNormalization(),
+            # 2nd upscale by fractionally strided conv, (32, 32, 128)
+            layers.Conv2DTranspose(
+                128, kernel_size=5, strides=2, padding="same", kernel_initializer=init,
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            layers.leakyReLU(alpha=0.2),
+            # 3rd upscale by fractionally strided conv, (64, 64, 64)
             layers.Conv2DTranspose(
-                128, kernel_size=5, strides=2, padding="same", activation="relu", use_bias=False
+                3, kernel_size=5, strides=2, padding="same", kernel_initializer=init,
             ),
             layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
-            layers.Conv2DTranspose(
-                3, kernel_size=5, strides=2, padding="same", activation="tanh", use_bias=False
-            ),
-            layers.BatchNormalization(),
-            # layers.leakyReLU(0.2),
+            layers.leakyReLU(alpha=0.2),
+            # Output, (64, 64, 3)
+            layers.Conv2D(3, kernel_size=7, padding="same", activation="tanh", kernel_initializer=init)
         ]
     )
     model.summary()
@@ -159,21 +167,26 @@ def discriminator_model_generic_deeper(
     """
     A generic discriminator model
     """
+    init = tf.keras.initializers.RandomNormal(stddev=0.02)
     model = tf.keras.Sequential(
         [
+            # Input size conv layer (e.g. 64, 64, 64)
             layers.Conv2D(
-                64, (5, 5), strides=(2, 2), padding="same", input_shape=input_shape
+                64, (5, 5), strides=(2, 2), padding="same", input_shape=input_shape, kernel_initializer=init
             ),
-            layers.LeakyReLU(0.2),
+            layers.LeakyReLU(alpha=0.2),
             layers.Dropout(0.3),
-            layers.Conv2D(128, (5, 5), strides=(2, 2), padding="same"),
-            layers.LeakyReLU(0.2),
+            # Half input size conv layer (e.g. 32, 32, 128)
+            layers.Conv2D(128, (5, 5), strides=(2, 2), padding="same", kernel_initializer=init),
+            layers.LeakyReLU(alpha=0.2),
             layers.Dropout(0.3),
-            layers.Conv2D(256, (5, 5), strides=(2, 2), padding="same"),
-            layers.LeakyReLU(0.2),
+            # Quarter input size conv layer (e.g. 16, 16, 256)
+            layers.Conv2D(256, (5, 5), strides=(2, 2), padding="same", kernel_initializer=init),
+            layers.LeakyReLU(alpha=0.2),
             layers.Dropout(0.3),
+            # Output neuron
             layers.Flatten(),
-            layers.Dense(1),
+            layers.Dense(1, activation="sigmoid"),
         ]
     )
 
